3f93cb51fc2df1bde7d8a28c15a599214151502b
AlexeyFroloff <45922538+AlexeyFroloff@users.noreply.github.com>
2023-04-12 22:17:02 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import numpy as np
-
+from scipy import stats
 
 chat_id = 123456 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
+    stat, p_value = stats.ks_2samp(x, y, alternative='two-sided')
+    alpha = 0.03
+    return p_value < alpha
+    
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    #return ... # Ваш ответ, True или False
